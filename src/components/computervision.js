424f9531ebc820466e41df4c6fbc00b9bfeeb4a0
klucas12345 <klucas12345@gmail.com>
2023-08-06 04:02:42 -0400

    cvc2

--- src/components/computervision.js
+++ src/components/computervision.js
@@ -21,6 +21,43 @@ const ComputerVision = () => {
     fetchImageData();
   }, []);
 
+  const handleImageUpload = async (event) => {
+    setLoading(true);
+    setError(null);
+
+    try {
+      const file = event.target.files[0];
+      const formData = new FormData();
+      formData.append('image', file);
+
+      const response = await axios.post('/api/uploadImage', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      setImageData(response.data);
+      setLoading(false);
+    } catch (error) {
+      setError(error.message);
+      setLoading(false);
+    }
+  };
+
+  const handleImageDelete = async () => {
+    setLoading(true);
+    setError(null);
+
+    try {
+      await axios.delete('/api/deleteImage');
+      setImageData(null);
+      setLoading(false);
+    } catch (error) {
+      setError(error.message);
+      setLoading(false);
+    }
+  };
+
   if (loading) {
     return <div>Loading...</div>;
   }
@@ -32,10 +69,12 @@ const ComputerVision = () => {
   return (
     <div>
       <h1>Computer Vision Component</h1>
+      <input type="file" onChange={handleImageUpload} />
       {imageData && (
         <div>
           <img src={imageData.imageUrl} alt="Image" />
           <p>Image Label: {imageData.label}</p>
+          <button onClick={handleImageDelete}>Delete Image</button>
         </div>
       )}
     </div>
